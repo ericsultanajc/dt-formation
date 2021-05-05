ca4dda743c357591acb858bda4e75284c8f3fa61
Baptiste Dessandier <baptiste.dessandier@gmail.com>
2021-05-05 17:00:51 +0200

    TEST V2

--- formation-model/src/test/java/sopra/formation/test/TestModel.java
+++ formation-model/src/test/java/sopra/formation/test/TestModel.java
@@ -4,13 +4,17 @@ package sopra.formation.test;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import sopra.formation.model.Adresse;
 import sopra.formation.model.Civilite;
 import sopra.formation.model.Dispositif;
 import sopra.formation.model.Evaluation;
 import sopra.formation.model.Filiere;
 import sopra.formation.model.Formateur;
+import sopra.formation.model.Matiere;
 import sopra.formation.model.NiveauEtude;
+import sopra.formation.model.Salle;
 import sopra.formation.model.Stagiaire;
+import sopra.formation.model.UE;
 
 public class TestModel {
 
@@ -26,6 +30,17 @@ public class TestModel {
 		Filiere filieresopra= new Filiere("J2EE","2021",dt.parse("12/04/2021 09:00"),3,Dispositif.POEI);
 		Baptiste.setFiliere(filieresopra);
 		Flo.setFiliere(filieresopra);
+		Adresse salleformasopra = new Adresse("20 avenue de Pythagore","","33700","Mérignac");
+		Adresse adrbat = new Adresse("45 rue des Carmélites","Appartement 15","86000","Poitiers");
+		Adresse adrflo = new Adresse("69 rue des SansDents","","59067","Bergues");
+		Adresse adreric = new Adresse ("56 rue des Lilas","","33449","Saint-Médard en Jalles");
+		Baptiste.setAdresse(adrbat);
+		Flo.setAdresse(adrflo);
+		eric.setAdresse(adreric);
+		Salle sallesopra = new Salle("Salle Schrödinger",25,false);
+		sallesopra.setAdresse(salleformasopra);
+		Matiere bddmysql = new Matiere();
+		UE bdd = new UE();
 	}
 
 }
