81e13cb3a36bf3cf92078071e8f3c59a2c1f5221
camille <c>
2021-05-06 14:41:15 +0200

    test
    fonction Write Camille

--- formation-dao/src/test/java/sopra/formation/test/TestWrite.java
+++ formation-dao/src/test/java/sopra/formation/test/TestWrite.java
@@ -3,15 +3,18 @@ package sopra.formation.test;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class TestWrite {
 
 	private static final String EVAL_FILENAME = "evaluations.txt";
+	private static final String EVAL_FILENAME2 = "evaluations2.txt";
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		String evaluation1 = "1;15;18;Très bon élément";
 
 		writeWithBuffered(evaluation1);
+		writeWithPrinteWriter(evaluation1);
 		
 	}
 
@@ -25,5 +28,13 @@ public class TestWrite {
 		}
 
 	}
+	
+	private static void writeWithPrinteWriter(String chaine) throws IOException {
+		FileWriter fileWriter= new FileWriter(EVAL_FILENAME2);
+		PrintWriter printWriter= new PrintWriter(fileWriter);
+		printWriter.print(chaine+"\n");
+		printWriter.close();
+	}
+		
 
 }
