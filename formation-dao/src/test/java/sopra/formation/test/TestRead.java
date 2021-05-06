631dd999da3d2da8780f46b7163f7e7798d5d55e
Baptiste Dessandier <baptiste.dessandier@gmail.com>
2021-05-06 15:13:42 +0200

    Commit test read avec inputstream

--- formation-dao/src/test/java/sopra/formation/test/TestRead.java
+++ formation-dao/src/test/java/sopra/formation/test/TestRead.java
@@ -1,5 +1,61 @@
 package sopra.formation.test;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestRead {
 
-}
+	private static final String EVAL_FILENAME = "evaluations.txt";
+	
+	public static void main(String[] args) {
+		File file = new File("evaluations.txt");
+		InputStream inputStream = null;
+		try {
+			try {
+				inputStream = new FileInputStream(file);
+				try {
+					System.out.println(readFromInputStream(inputStream));
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+
+		} finally {
+			if (inputStream != null) {
+				try {
+					inputStream.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
+	private static List<String> readWithBufferedReader() {
+		List<String> lines = new ArrayList<String>();
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(EVAL_FILENAME))) {
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				lines.add(line);
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		return lines;
+	}
+	
+s
