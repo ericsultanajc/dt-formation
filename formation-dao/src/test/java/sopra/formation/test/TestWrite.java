bb1d6a726f36320a628c9682bbacb21e958cef9a
Lea Dumont <leadumont2512@gmail.com>
2021-05-06 14:44:56 +0200

    fcts write

--- formation-dao/src/test/java/sopra/formation/test/TestWrite.java
+++ formation-dao/src/test/java/sopra/formation/test/TestWrite.java
@@ -1,8 +1,12 @@
 package sopra.formation.test;
 
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 
 public class TestWrite {
 
@@ -11,7 +15,14 @@ public class TestWrite {
 	public static void main(String[] args) {
 		String evaluation1 = "1;15;18;Très bon élément";
 
-		writeWithBuffered(evaluation1);
+		//writeWithBuffered(evaluation1);
+		//writeWithPrintWriter(evaluation1);
+		
+		try {
+			writeWithFileOutputStream(evaluation1);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
@@ -24,6 +35,25 @@ public class TestWrite {
 			e.printStackTrace();
 		}
 
+	}
+	
+	private static void writeWithPrintWriter(String chaine) {
+		try (FileWriter writer = new FileWriter(EVAL_FILENAME, true)) {
+			PrintWriter printWriter = new PrintWriter(writer);
+			printWriter.print(chaine+"\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void writeWithFileOutputStream(String chaine) throws IOException {
+		try (FileOutputStream writer = new FileOutputStream(EVAL_FILENAME, true)) {
+			chaine += "\n";
+			byte[] strToBytes = (chaine+"\n").getBytes();
+			writer.write(strToBytes);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
