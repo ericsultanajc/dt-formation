92f41192a34069c4950886199a6964e16be08289
Baptiste Dessandier <baptiste.dessandier@gmail.com>
2021-05-06 15:15:50 +0200

    bug du précédent test read

--- formation-dao/src/test/java/sopra/formation/test/TestRead.java
+++ formation-dao/src/test/java/sopra/formation/test/TestRead.java
@@ -58,4 +58,14 @@ public class TestRead {
 		return lines;
 	}
 	
-s
+	private static String readFromInputStream(InputStream inputStream) throws IOException {
+		StringBuilder resultStringBuilder = new StringBuilder();
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				resultStringBuilder.append(line).append("\n");
+			}
+		}
+		return resultStringBuilder.toString();
+	}
+}
