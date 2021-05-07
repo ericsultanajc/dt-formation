8eb4c46ba6a3b9ec94dd55850e71f9415075a57e
Seif <seif@>
2021-05-07 14:25:17 +0200

    stagiaire

---
+++ formation-dao/src/main/java/sopra/formation/dao/file/csv/StagiaireDaoCsv.java
@@ -0,0 +1,182 @@
+package sopra.formation.dao.file.csv;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+import sopra.formation.dao.IStagiaireDao;
+import sopra.formation.model.Matiere;
+import sopra.formation.model.NiveauEtude;
+import sopra.formation.model.Stagiaire;
+
+public class StagiaireDaoCsv implements IStagiaireDao {
+	
+	private final String fileName;
+	private final String separator = ";";
+
+	public StagiaireDaoCsv(String fileName) {
+		super();
+		this.fileName = fileName;
+	}
+	
+	public List<Stagiaire> findAll() {
+		return read();
+	}
+	
+	public Stagiaire findById(Long id) {
+		List<Stagiaire> stagiaires = read();
+
+		for (Stagiaire stagiaire : stagiaires) {
+			if (stagiaire.getId() == id) {
+				return stagiaire;
+			}
+		}
+		return null;
+	}
+	
+	public void create(Stagiaire obj) {
+		
+		List<Stagiaire> stagiaires = read();
+
+		Long maxId = 0L;
+		for (Stagiaire sta : stagiaires) {
+			if (maxId < sta.getId()) {
+				maxId = sta.getId();
+			}
+		}
+
+		obj.setId(++maxId);
+
+		stagiaires.add(obj);
+
+		write(stagiaires);
+	}
+
+	public void update(Stagiaire obj) {
+		
+		List<Stagiaire> stagiaires = read();
+
+		int index = 0;
+		boolean find = false;
+
+		for (Stagiaire sta : stagiaires) {
+			if (sta.getId() == obj.getId()) {
+				find = true;
+				break;
+			}
+
+			index++;
+		}
+		
+		if (find) {
+			stagiaires.set(index, obj);
+
+			write(stagiaires);
+		}
+	}
+	
+	public void delete(Stagiaire obj) {
+		deleteById(obj.getId());
+	}
+	
+	public void deleteById(Long id) {
+		
+		List<Stagiaire> stagiaires = read();
+		
+		int index = 0;
+		boolean find = false;
+
+		for (Stagiaire sta : stagiaires) {
+			if (sta.getId() == id) {
+				find = true;
+				break;
+			}
+
+			index++;
+		}
+		
+		if (find) {
+			stagiaires.remove(index);
+
+			write(stagiaires);
+		}
+	}
+	
+	
+	private List<Stagiaire> read() {
+		List<Stagiaire> stagiaires = new ArrayList<Stagiaire>();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		Path path = Paths.get(this.fileName);
+
+		try {
+			List<String> lines = Files.readAllLines(path);
+
+			for (String line : lines) {
+				String[] items = line.split(this.separator);
+
+				Long id = Long.valueOf(items[0]);
+				String civilite =items[1];
+				String nom = items[2];
+				String prenom = items[3];
+				String email = items[4];
+				String telephone = items[5];
+				try {
+					Date dtNaissance= sdf.parse(items[6]);
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				NiveauEtude niveauEtude = NiveauEtude.valueOf(items[7]);
+
+				Stagiaire stagiaire = new Stagiaire(id,civilite);
+
+				stagiaires.add(stagiaire);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return stagiaires;
+	}
+	
+	private void write(List<Stagiaire> stagiaires) {
+		List<String> lines = new ArrayList<String>();
+
+		for (Stagiaire stagiaire : stagiaires) {
+			StringBuilder line = new StringBuilder();
+			line.append(stagiaire.getId());
+			line.append(this.separator);
+			line.append(stagiaire.getCivilite());
+			line.append(this.separator);
+			line.append(stagiaire.getNom());
+			line.append(this.separator);
+			line.append(stagiaire.getPrenom());
+			line.append(this.separator);
+			line.append(stagiaire.getEmail());
+			line.append(this.separator);
+			line.append(stagiaire.getTelephone());
+			line.append(this.separator);
+			line.append(stagiaire.getDtNaissance());
+			line.append(this.separator);
+			line.append(stagiaire.getNiveauEtude());
+
+			lines.add(line.toString());
+		}
+
+		Path path = Paths.get(this.fileName);
+
+		try {
+			Files.write(path, lines);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
--- formation-dao/src/test/java/sopra/formation/test/TestDao.java
+++ formation-dao/src/test/java/sopra/formation/test/TestDao.java
@@ -3,33 +3,65 @@ package sopra.formation.test;
 import java.util.List;
 
 import sopra.formation.dao.IEvaluationDao;
+import sopra.formation.dao.IMatiereDao;
+import sopra.formation.dao.IStagiaireDao;
 import sopra.formation.dao.file.csv.EvaluationDaoCsv;
+import sopra.formation.dao.file.csv.MatiereDaoCsv;
+import sopra.formation.dao.file.csv.StagiaireDaoCsv;
 import sopra.formation.model.Evaluation;
+import sopra.formation.model.Matiere;
+import sopra.formation.model.Stagiaire;
 
 public class TestDao {
 
 	public static void main(String[] args) {
 		IEvaluationDao evaluationDao = new EvaluationDaoCsv("evaluations.txt");
+		IMatiereDao matiereDAO = new MatiereDaoCsv("matieres.txt");
+		IStagiaireDao stagiaireDao = new StagiaireDaoCsv("stagiaire.txt");
 
+		
 		List<Evaluation> evaluations = evaluationDao.findAll();
 
 		for (Evaluation evaluation : evaluations) {
 			System.out.println(evaluation);
 		}
+		
+		
+		List<Matiere> matieres=matiereDAO.findAll();
+		
+		for (Matiere matiere : matieres) {
+			System.out.println(matieres);
+		}
+		
+		List<Stagiaire> stagiaires=stagiaireDao.findAll();
+		
+		for (Matiere matiere : matieres) {
+			System.out.println(matieres);
+		}
 
 		System.out.println(evaluationDao.findById(5L));
+		
+		System.out.println(matiereDAO.findById(1L));
 
 		Evaluation evaluation = new Evaluation(14, 12, "Peut mieux faire");
+		Matiere matiere = new Matiere(3L,"SQL", 30);
 
 		evaluationDao.create(evaluation);
+		matiereDAO.create(matiere);
 
 		evaluation.setComportemental(18);
 		evaluation.setTechnique(15);
 		evaluation.setCommentaires("Grosse amélioration");
-
+	
 		evaluationDao.update(evaluation);
 
 		evaluationDao.delete(evaluation);
+		
+		matiere.setDuree(3);
+		matiere.setId(4L);
+		matiere.setNom("Maven");
+		matiereDAO.update(matiere);
+		matiereDAO.delete(matiere);
 	}
 
 }
